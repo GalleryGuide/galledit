444cd94a6169abab0b43908fedda29c2161dc184
Malcolm Young <malcolm@rubydesign.co.uk>
2017-03-12 08:44:06 +0000

    hide address form initially, show it after selecting from map

---
+++ js/galledit.js
@@ -0,0 +1,17 @@
+(function ($, Drupal) {
+  /**
+   * Disable address form until map entry complete.
+   */
+  Drupal.behaviors.gallAddress = {
+    attach: function (context, settings) {
+      var $addressWrapper = $('#edit-field-address-wrapper');
+      
+      $addressWrapper.css('visibility', 'hidden');
+      
+      $('.geocode-controls-wrapper input').change(function() {
+        $addressWrapper.css('visibility', 'visible');
+      });
+    }
+  };
+
+})(jQuery, Drupal);
