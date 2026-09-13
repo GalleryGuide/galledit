779d88015a735b2347781c4f59836a03884b6ac6
Malcolm Young <428782+malcomio@users.noreply.github.com>
2026-09-13 08:47:47 +0100

    switch to dart-sass (#23)
    * switch to dart-sass
    
    * compiled CSS
    
    * remove gulp-scss-lint
    
    * remove unnecessary dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,55 @@
-const {src, dest, watch, series, parallel} = require('gulp');
+const { src, dest, watch, series } = require("gulp");
+const plumber = require("gulp-plumber");
+const $ = require("gulp-load-plugins")();
+const sass = require("gulp-sass")(require("sass"));
+const bs = require("browser-sync").create();
 
-// Gulp requires.
-var gulp = require('gulp'),
-  $ = require('gulp-load-plugins')(),
-  minimist = require('minimist'),
-  arg = minimist(process.argv.slice(2));
+/**
+ * Compile Sass to CSS.
+ *
+ * @return {*} A stream.
+ */
+const buildStyles = () =>
+  src("sass/galledit.scss")
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(dest("css/"));
 
-var sass = require('gulp-sass');
-
-sass.compiler = require('node-sass');
-
-// Default environment options.
-var envOption = {
-  minify: false,
-  sourcemap: true,
-  styleGuide: true
+/**
+ * Initialise browser-sync to proxy the site.
+ */
+const browserSyncServe = () => {
+  buildStyles();
+  bs.init({
+    // Dev server will run at localhost:8080
+    proxy: "localhost",
+    port: 8080
+  });
 };
 
-// Set environment options. Vagrant uses defaults.
-switch (arg.env) {
-  case 'production':
-    envOption.minify = true;
-    envOption.sourcemap = false;
-    envOption.styleGuide = false;
-    break;
-}
+/**
+ * Trigger a reload via browser-sync.
+ *
+ * @param callback The callback function.
+ */
+const browsersyncReload = (callback) => {
+  bs.reload();
+  callback();
+};
 
-// Use Node Sass (LibSass) to compile Sass.
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+/**
+ * Watch Sass files and build when they change.
+ */
+const watchFiles = () => {
+  browserSyncServe();
+  watch(
+    ["scss/**/*.scss"],
+    { events: "all", ignoreInitial: false },
+    series(buildStyles, browsersyncReload)
+  );
+};
 
-    .pipe($.if(envOption.sourcemap, $.sourcemaps.init()))
-    .pipe($.sass())
-    .pipe($.autoprefixer('last 2 versions', 'ie 8', 'ie 9'))
-    // Optionally produce production CSS.
-    .pipe($.if(envOption.sourcemap, $.sourcemaps.write('./')))
-    .pipe($.if(envOption.minify, $.minifyCss()))
-    .pipe(dest('css'))
-
-    .pipe(gulp.dest('./css'));
-});
-
-// SCSS lint.
-gulp.task('scss-lint', function () {
-  return gulp.src('sass/**/*.scss')
-    .pipe($.cached($.scssLint))
-    .pipe($.scssLint({
-      'config': '../gall/scss-lint.yml'
-    }));
-});
-
-gulp.task('build', gulp.series('sass'));
-
-// Default - initial compile and watch.
-gulp.task('default', gulp.series('sass', 'scss-lint'));
+exports.styles = series(buildStyles);
+exports.build = series(buildStyles);
+exports.watch = watchFiles;
+exports.serve = browserSyncServe;
